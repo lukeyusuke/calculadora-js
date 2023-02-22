b87e0ce3582ab8a069133c1f4778cd56d8d8487a
lukeyusuke <lukedev09@gmail.com>
2023-02-22 17:06:04 -0300

    Adiciona evento de clique
    - forEach para pegar o valor do botão e mostrar na tela

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// pega os elementos que vamos mexer
+function calculadora(){
+    const inputCalculo = document.querySelector('.calc');
+    const buttons = document.querySelectorAll('.btn');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            inputCalculo.value += button.value;
+        })
+    })
+}
+
+calculadora();
+
+// Ter o evento de clique em cada um dos botões através do forEach
+// Descobrir se o botão é uma operação ou não

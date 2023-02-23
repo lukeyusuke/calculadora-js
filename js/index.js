261156d250b68de44d670eb5d76fccf83aea76a3
lukeyusuke <lukedev09@gmail.com>
2023-02-23 16:18:52 -0300

    Função que mostra e deleta os números no Input

--- js/index.js
+++ js/index.js
@@ -1,10 +1,39 @@
 // Pega os elementos que vamos utilizar
-
-const previousOperand = document.querySelector('[data-previous-operand]');
-const currentOperand = document.querySelector('[data-current-operand]');
+const inputCalculo = document.querySelector('[data-input]')
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
 
 const numberBtn = document.querySelectorAll('[data-number]');
 const operatorBtn = document.querySelectorAll('[data-operator]');
 const deleteBtn = document.querySelector('[data-delete]');
 const clearBtn = document.querySelector('[data-clear]');
 const equalsBtn = document.querySelector('[data-equals]');
+
+class Calculator {
+    constructor(previousOperandText, currentOperandText){
+        this.previousOperandText = previousOperandText;
+        this.previousOperandText = currentOperandText;
+    }
+
+    showValue(valor){
+        inputCalculo.value += valor;
+    }
+
+    clear(){
+        inputCalculo.value = ""; 
+        this.previousOperandText = "";
+        this.currentOperandText = "";
+    }
+}
+
+for(const number of numberBtn){
+    number.addEventListener('click', () => {
+        calculator.showValue(number.innerText);
+    })
+}
+
+clearBtn.addEventListener('click', () => {
+    calculator.clear();
+})
+
+const calculator = new Calculator(previousOperandText, currentOperandText);

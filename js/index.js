04c3168acb144e6bf2e1a2867402e10dc566cfbb
lukeyusuke <lukedev09@gmail.com>
2023-02-27 15:32:43 -0300

    Cria evento que chama o método showOutput
    Chamada do evento showOutput, que exibe os botões clicados na tela

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const btnNumber = document.querySelectorAll('[data-number]');
-const btnOperation = document.querySelectorAll('[data-operation]');
+const btnOperator = document.querySelectorAll('[data-operator]');
 const btnDelete = document.querySelector('[data-delete]')
 const btnAllClear = document.querySelector('[data-all-clear]');
 const btnEquals = document.querySelector('[data-equals]');
@@ -7,12 +7,36 @@ const btnEquals = document.querySelector('[data-equals]');
 const previousOperand = document.querySelector('[data-previous-operand]'); 
 const currentOperand = document.querySelector('[data-current-operand]');
 
-class calculator {
-    constructor(previousOperandText, currentOperandText){
-        this.previousOperandText = previousOperandText;
-        this.currentOperandText = currentOperandText;
+class Calculator {
+    constructor(previousOperand, currentOperand){
+        this.previousOperand = previousOperand;
+        this.currentOperand = currentOperand;
+        this.clear();
+    }
+
+    clear(){
+        this.previousOperand.innerText = "";
+        this.currentOperand.innerText = "";
+    }
+
+    showOutput(number){
+        if (this.currentOperand.innerText.includes(".") && number === ".") return;
+        this.currentOperand.innerText += number;
+        // identificar quando clicamos no +, - , x, dividir
     }
 }
 
-const calc = calculator(previousOperand, currentOperand);
+const calculator = new Calculator(previousOperand, currentOperand);
 
+// identificar o botão que estamos clicando
+
+for(const number of btnNumber){
+    number.addEventListener('click', () => {
+        calculator.showOutput(number.innerText);
+    })
+}
+
+// função que deleta o valor
+btnAllClear.addEventListener('click', () => {
+    calculator.clear();
+})

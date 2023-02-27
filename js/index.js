c52bbed114ba95098d1f42e1518708a0a92e58e5
lukeyusuke <lukedev09@gmail.com>
2023-02-27 13:28:23 -0300

    Pega os elementos do HTML para trablhar em JS

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// pega os elementos que vou manipular
+const btnNumber = document.querySelectorAll('[data-number]');
+const btnOperation = document.querySelectorAll('[data-operation]');
+const btnDelete = document.querySelector('[data-delete]')
+const btnAllClear = document.querySelector('[data-all-clear]');
+const btnEquals = document.querySelector('[data-equals]');
+
+const previousOperand = document.querySelector('[data-previous-operand]'); 
+const currentOperand = document.querySelector('[data-current-operand]');
+
+
+
+

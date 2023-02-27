9c51c791e6abc65ecbba6a4f5c957e286cd54e28
lukeyusuke <lukedev09@gmail.com>
2023-02-27 14:57:09 -0300

    Evento que identifica o clique do botão

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-const btnNumber = document.querySelectorAll('[data-number]');
-const btnOperation = document.querySelectorAll('[data-operation]');
+const buttons = document.querySelectorAll('[data-button]');
 const btnDelete = document.querySelector('[data-delete]')
 const btnAllClear = document.querySelector('[data-all-clear]');
 const btnEquals = document.querySelector('[data-equals]');
@@ -7,12 +6,26 @@ const btnEquals = document.querySelector('[data-equals]');
 const previousOperand = document.querySelector('[data-previous-operand]'); 
 const currentOperand = document.querySelector('[data-current-operand]');
 
-class calculator {
-    constructor(previousOperandText, currentOperandText){
-        this.previousOperandText = previousOperandText;
-        this.currentOperandText = currentOperandText;
+class Calculator {
+    constructor(previousOperand, currentOperand){
+        this.previousOperand = previousOperand;
+        this.currentOperand = currentOperand;
+    }
+
+    clear(){
+        this.previousOperand.innerText = "";
+        this.currentOperand.innerText = "";
+    }
+
+    showOutput(button){
+        this.currentOperand.innerText += button;
     }
 }
 
-const calc = calculator(previousOperand, currentOperand);
+const calculator = new Calculator(previousOperand, currentOperand);
 
+for(const button of buttons){
+    button.addEventListener('click', () => {
+        calculator.showOutput(button.innerText);
+    })
+}
